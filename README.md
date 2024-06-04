6953fea78620d64ab8176239f031ad468dc895ec
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-04 23:02:23 +0300

    loop-task

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # loop-task
+# JavaScript Task: Various methods were utilized to accomplish the task.
+- Loop
+- console 
+- alert
---
+++ app.js
@@ -0,0 +1,43 @@
+// Q7:
+
+    // for (let i = 0; i<=5; i++) {
+    //     alert(i);}
+
+// Q8:
+
+    // let num=0
+    // for (let i = 0; i<=5; i++) {
+    //      num = num + i + "\n"
+    //     }
+    //     alert(num);
+
+// Q9:
+
+    // for (let i = 0; i<=20; i+=3) {
+    //     alert(i);}
+
+// Q10:
+
+    // var i= prompt('enter number');
+    // i = parseFloat(i)
+    //     for (var i;i < 1 || i > 101; i++)
+    //    { 
+    //     alert("error");
+    //     var i= prompt('Error: Please enter a valid number between 0 and 100');}
+
+// Q11:  
+
+    // let num= prompt('enter number');
+    // num = parseInt(num);
+    // sum=0;
+    // for (let i=0; i <= num; i++) {
+    //     sum = sum + i;}
+    //     alert(sum);
+
+
+
+
+
+
+
+
